105f98e75a66c976674efec0e8cf880420bb412e
Cynthia Alberca <cynthiaalberca17@gmail.com>
2025-10-23 05:29:01 +0800

    added LEAKY_RELU_SLOPE to utils.py to avoid duplication

--- thesis_models/utils.py
+++ thesis_models/utils.py
@@ -1,6 +1,13 @@
 import torch
 import torch.nn as nn
 
+
+# Empirically, 0.1 worked best in GAN-based vocoders for audio stability
+# The small negative slope (0.1) allows a small gradient to flow even for negative activations
+# Helpful in preventing "dead" neurons
+LEAKY_RELU_SLOPE = 0.1
+
+
 def init_weights(module: nn.Module, mean: float = 0.0, std: float = 0.01):
     """
     Initializes convolutional weights with values from the normal distribution (mean = 0, std = 0.01)
@@ -16,3 +23,8 @@ def init_weights(module: nn.Module, mean: float = 0.0, std: float = 0.01):
 
     if classname.find("Conv") != -1:
         module.weight.data.normal_(mean, std)
+
+
+def get_padding(kernel_size: int, dilation: int = 1) -> int:
+    """Compute symmetric padding for 1D convs."""
+    return int((kernel_size * dilation - dilation) // 2)
